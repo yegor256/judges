c3a15897b3b019fd99f71cd8c727bab826a8506b
Yegor Bugayenko <yegor256@gmail.com>
2025-01-03 12:31:09 +0300

    year up to 2025

--- lib/judges/churn.rb
+++ lib/judges/churn.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Copyright (c) 2024 Yegor Bugayenko
+# Copyright (c) 2024-2025 Yegor Bugayenko
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the 'Software'), to deal
@@ -24,7 +24,7 @@ require_relative '../judges'
 
 # How many facts were modified.
 # Author:: Yegor Bugayenko (yegor256@gmail.com)
-# Copyright:: Copyright (c) 2024 Yegor Bugayenko
+# Copyright:: Copyright (c) 2024-2025 Yegor Bugayenko
 # License:: MIT
 class Judges::Churn
   attr_reader :added, :removed, :errors
