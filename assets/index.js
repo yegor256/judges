6d3339fa11ff61ea3b7a1812c72480cfb72e4ba8
rultor <me@rultor.com>
2025-04-05 04:22:38 +0000

    New JS and CSS

--- assets/index.js
+++ assets/index.js
@@ -1,1 +1,17 @@
-test
+/*
+ SPDX-FileCopyrightText: Copyright (c) 2024-2025 Yegor Bugayenko
+ SPDX-License-Identifier: MIT
+*/
+
+$(() => {
+  $('#facts').tablesorter();
+});
+
+updateTime = () => {
+  const now = new Date();
+  const isoTime = now.toISOString();
+  const timeElement = document.getElementById('current-time');
+  timeElement.textContent = `Current time: ${isoTime}`;
+}
+updateTime();
+setInterval(updateTime, 1000);
