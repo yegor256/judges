0cf8d6f9a0f18c9c3e8c8132d8d0563c9cda617a
Yegor Bugayenko <yegor256@gmail.com>
2025-04-07 19:57:40 +0800

    skip picks

--- test/test__helper.rb
+++ test/test__helper.rb
@@ -7,32 +7,15 @@ $stdout.sync = true
 
 require 'simplecov'
 require 'simplecov-cobertura'
-unless SimpleCov.running
-  # Custom formatter
-  class MyFormatter
-    def format(result)
-      puts 'Coverage:'
-      result.files.sort_by(&:filename).each do |file|
-        per = file.covered_percent
-        # rubocop:disable Style/FormatStringToken
-        puts Kernel.format(
-          '%40s %7.2f%% %s',
-          File.basename(file.filename),
-          per, (' (!)' if per < 80)
-        )
-        # rubocop:enable Style/FormatStringToken
-      end
-    end
-  end
+unless SimpleCov.running || ENV['PICKS']
   SimpleCov.command_name('test')
   SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
     [
       SimpleCov::Formatter::HTMLFormatter,
-      SimpleCov::Formatter::CoberturaFormatter,
-      MyFormatter
+      SimpleCov::Formatter::CoberturaFormatter
     ]
   )
-  SimpleCov.minimum_coverage 90
+  SimpleCov.minimum_coverage 80
   SimpleCov.minimum_coverage_by_file 80
   SimpleCov.start do
     add_filter 'test/'
@@ -44,7 +27,6 @@ unless SimpleCov.running
 end
 
 require 'minitest/autorun'
-
 require 'minitest/reporters'
 Minitest::Reporters.use! [Minitest::Reporters::SpecReporter.new]
 
