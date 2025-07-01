cbc08bb7c1f71f11354d81c2c770e4cf143c4383
Rultor.com <gpg@rultor.com>
2025-07-01 07:36:04 +0000

    version set to 0.50.7

--- lib/judges.rb
+++ lib/judges.rb
@@ -8,5 +8,5 @@
 # Copyright:: Copyright (c) 2024-2025 Yegor Bugayenko
 # License:: MIT
 module Judges
-  VERSION = '0.0.0' unless const_defined?(:VERSION)
+  VERSION = '0.50.7' unless const_defined?(:VERSION)
 end
