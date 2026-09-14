e95b561830b1276560006ec66ef9417c94eb91a3
gemshrine <113778529+gemshrine@users.noreply.github.com>
2026-09-14 20:43:24 +0300

    Import arrays as multiple fact values

--- lib/judges/commands/import.rb
+++ lib/judges/commands/import.rb
@@ -43,7 +43,9 @@ class Judges::Import
       yaml.each do |i|
         f = fb.insert
         i.each do |p, v|
-          f.public_send(:"#{p}=", v)
+          Array(v).each do |value|
+            f.public_send(:"#{p}=", value)
+          end
         end
       end
       impex.export(fb)
