a8c16198ffe7f42b0f752e35d28cb1e3384f2d0f
gemshrine <113778529+gemshrine@users.noreply.github.com>
2026-09-14 20:46:36 +0300

    Preserve scalar YAML values during import

--- lib/judges/commands/import.rb
+++ lib/judges/commands/import.rb
@@ -43,7 +43,7 @@ class Judges::Import
       yaml.each do |i|
         f = fb.insert
         i.each do |p, v|
-          Array(v).each do |value|
+          (v.is_a?(Array) ? v : [v]).each do |value|
             f.public_send(:"#{p}=", value)
           end
         end
